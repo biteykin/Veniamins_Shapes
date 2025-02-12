fb590e6a5904dfb28429eaa4229b3d9832809386
ivanbit <biteykin@users.noreply.github.com>
2025-02-12 11:48:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Подключение к БД на Railway
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@host:port/dbname")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:RiFCVtQKPvaTSqFqGKxDUJtlCVfRanql@monorail.proxy.rlwy.net:58544/railway")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
